ed59f1fca01f106de7dff77f69a7e2472758703d
Nehemias Feliz Ferreras <juniornff@gmail.com>
2021-12-11 03:41:17 -0400

    Intentos y cambios

--- Controllers/Asientos_ContablesController.cs
+++ Controllers/Asientos_ContablesController.cs
@@ -4,8 +4,12 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using PPFF.Models;
 
 namespace PPFF.Controllers
@@ -48,16 +52,58 @@ namespace PPFF.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Descripcion,ID_Cliente,Cuenta_contable,Tipo_Movimiento,Fecha,Monto,Estado")] Asientos_Contables asientos_Contables)
+        public async Task<ActionResult> Create([Bind(Include = "ID,Descripcion,ID_Cliente,Cuenta_contable,Tipo_Movimiento,Fecha,Monto,Estado")] Asientos_Contables asientos_Contables)
         {
             if (ModelState.IsValid)
             {
-                db.Asientos_Contables.Add(asientos_Contables);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                Body body = new Body();
+                body.description = asientos_Contables.Descripcion;
+                body.auxiliar = 3;
+                body.currencyCode = 1;
+                detail detail = new detail();
+                detail.cuentaCR = "6";
+                detail.cuentaDB = "13";
+                detail.amountCR = int.Parse(asientos_Contables.Monto.ToString());
+                detail.amountDB = int.Parse(asientos_Contables.Monto.ToString());
+                body.detail = detail;
+
+
+                //HttpClient client = new HttpClient();
+                //var values = body;
+                //var content = new FormUrlEncodedContent(values);
+                //var response = await client.PostAsync("http://www.example.com/recepticle.aspx", content);
+                //var responseString = await response.Content.ReadAsStringAsync();
+
+                string Baseurl = "https://accountingaccountapi20211205021409.azurewebsites.net/";
+                    var client = new HttpClient();
+                    List<Body> bodis = new List<Body>();
+                        bodis.Add(body);
+                        //Passing service base url
+                        client.BaseAddress = new Uri(Baseurl);
+                        client.DefaultRequestHeaders.Clear();
+                        //Define request data format
+                        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        HttpContent content = new StringContent(JsonConvert.SerializeObject(bodis));
+                        //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+                        HttpResponseMessage Res = await client.PostAsync("api/AccountingSeat/Register", content);
+                        //Checking the response is successful or not which is sent using HttpClient
+                        if (Res.IsSuccessStatusCode)
+                        {
+                            //Storing the response details recieved from web api
+                            var Response = Res.Content.ReadAsStringAsync().Result;
+                            //Deserializing the response recieved from web api and storing into the Employee list
+                            resp Respu = JsonConvert.DeserializeObject<resp>(Response);
+
+                            asientos_Contables.ID = Respu.value;
+
+                            db.Asientos_Contables.Add(asientos_Contables);
+                            db.SaveChanges();
+                            return RedirectToAction("Index");
+                        }
             }
 
             ViewBag.ID_Cliente = new SelectList(db.Clientes, "ID", "Nombre", asientos_Contables.ID_Cliente);
+            ViewBag.Error = "Error en el guardado";
             return View(asientos_Contables);
         }
 
@@ -128,5 +174,27 @@ namespace PPFF.Controllers
             }
             base.Dispose(disposing);
         }
+    }
+
+    public class Body
+    {
+        public string description { get; set; }
+        public int auxiliar { get; set; }
+        public int currencyCode { get; set; }
+        public detail detail { get; set; }
+    }
+
+    public class detail
+    {
+        public string cuentaCR { get; set; }
+        public string cuentaDB { get; set; }
+        public int amountCR { get; set; }
+        public int amountDB { get; set; }
+    }
+
+    public class resp
+    {
+        public string id { get; set; }
+        public int value { get; set; }
     }
 }
